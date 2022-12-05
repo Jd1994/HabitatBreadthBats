b752f4d320fccd3652da9c682279ee0ecc2ef330
Jd1994 <jorge.carballo301094@gmail.com>
2022-12-05 10:10:49 -0600

    test commit #4

--- code/HabitatBreadthBats.R
+++ code/HabitatBreadthBats.R
@@ -2,6 +2,12 @@
 #Carballo-Morales et al. 2023
 
 #package required
+library("phytools")
+
+
 
 #raw data
-read.csv("raw_data/AppendixS1.csv") ## HB, mass, TG, and ER raw data
+data <- read.csv("raw_data/AppendixS1.csv") ## HB, mass, TG, and ER raw data
+
+#Phylogenetic tree
+tree <- read.tree("code/RAxML_bestTree.T1")
